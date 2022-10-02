b999d997952221dd8d9215d83121c16941c23d9d
Jennifer Bamanya <jennifer.bamanya@bp.com>
2022-10-02 20:16:42 +0200

    interactive session

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,24 @@
+import json
 from django.http import JsonResponse
 
+# request -> HttpRequest -> Django
+# print(dir(request))
+# request.body [JSON Data]
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    # print(dir(request))
+    print(request.GET) # url query params
+    print(request.POST) # 
+    body = request.body  # byte string of JSON data
+    data = {}
+    try:   # Use try block incase body does not have any JSON data
+        data = json.loads(body) # This takes in a string of JSON data and returns a Python Dictionary
+    except:
+        pass
+    print(data)
+    #print(data.keys())
+    # print(request.headers)
+    data['params'] = dict(request.GET)
+    data['Headers'] = dict(request.headers)
+    data['Content_Type'] = request.content_type
+    return JsonResponse(data)
